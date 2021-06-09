d9a0d65478c6f4e01b90f4cf89010a9add1496af
oadmin <kara-91@tut.by>
2021-06-09 10:06:32 +0300

    init : init project

---
+++ abw-kafka-producer/src/test/java/com/example/abwkafkaproducer/AbwKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abwkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbwKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
